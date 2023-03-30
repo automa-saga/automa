fec7aa51bd19c7ee0842858112ea1eaef7a22eaf
Lenin Mehedy <lenin.mehedy@swirldslabs.com>
2023-03-31 08:46:44 +1100

    fix: cleanup usage example
    Signed-off-by: Lenin Mehedy <lenin.mehedy@swirldslabs.com>

--- example/example.go
+++ example/example.go
@@ -3,8 +3,8 @@ package main
 import (
 	"context"
 	"fmt"
+	"github.com/automa-saga/automa"
 	"github.com/cockroachdb/errors"
-	"github.com/leninmehedy/automa"
 	"go.uber.org/zap"
 )
 
@@ -13,6 +13,29 @@ import (
 // This doesn't use mutex so shouldn't be used with coroutines
 // This is just for example purposes.
 type InMemCache map[string]interface{}
+
+// GetString returns the string value for the given key
+func (ic InMemCache) GetString(key string) string {
+	if s, ok := ic[key].(string); ok {
+		return s
+	}
+
+	return ""
+}
+
+// SetString returns the string value for the given key
+func (ic InMemCache) SetString(key string, val interface{}) error {
+	s, ok := val.(string)
+	if ok {
+		ic[key] = s
+	}
+
+	return errors.Newf("Could not cast the value to string: %v", ok)
+}
+
+const (
+	keyRollbackMsg = "rollbackMsg"
+)
 
 type StopContainers struct {
 	automa.Step
@@ -43,46 +66,44 @@ type RestartContainers struct {
 func (s *StopContainers) Run(ctx context.Context, prevSuccess *automa.Success) (automa.Reports, error) {
 	report := automa.NewReport(s.ID)
 	s.logger.Debug(fmt.Sprintf("RUN - %q", s.ID))
-	s.cache["rollbackMsg"] = fmt.Sprintf("ROLLBACK - %q", s.ID)
+	s.cache.SetString(keyRollbackMsg, fmt.Sprintf("RUN - %q", s.ID))
+
 	return s.RunNext(ctx, prevSuccess, report)
 }
 
 func (s *StopContainers) Rollback(ctx context.Context, prevFailure *automa.Failure) (automa.Reports, error) {
 	report := automa.NewReport(s.ID)
-	s.logger.Debug(s.cache["rollbackMsg"].(string))
+	s.logger.Debug(s.cache.GetString(keyRollbackMsg))
 	return s.RollbackPrev(ctx, prevFailure, report)
 }
 
 func (s *FetchLatest) Run(ctx context.Context, prevSuccess *automa.Success) (automa.Reports, error) {
 	s.logger.Debug(fmt.Sprintf("RUN - %q", s.ID))
-	s.cache["rollbackMsg"] = fmt.Sprintf("ROLLBACK - %q", s.ID)
+	s.cache.SetString(keyRollbackMsg, fmt.Sprintf("RUN - %q", s.ID))
 
 	return s.RunNext(ctx, prevSuccess, nil)
 }
 
 func (s *FetchLatest) Rollback(ctx context.Context, prevFailure *automa.Failure) (automa.Reports, error) {
 	report := automa.NewReport(s.ID)
-	s.logger.Debug(s.cache["rollbackMsg"].(string))
+	s.logger.Debug(s.cache.GetString(keyRollbackMsg))
 	return s.RollbackPrev(ctx, prevFailure, report)
 }
 
 func (s *NotifyAll) Run(ctx context.Context, prevSuccess *automa.Success) (automa.Reports, error) {
-	s.logger.Debug(fmt.Sprintf("SKIP RUN- %q", s.ID))
-	s.cache["rollbackMsg"] = []byte(fmt.Sprintf("SKIP ROLLBACK - %q", s.ID))
+	s.cache.SetString(keyRollbackMsg, fmt.Sprintf("RUN - %q", s.ID))
 	return s.SkippedRun(ctx, prevSuccess, nil)
 }
 
 func (s *NotifyAll) Rollback(ctx context.Context, prevFailure *automa.Failure) (automa.Reports, error) {
 	report := automa.NewReport(s.ID)
-	s.logger.Debug(s.cache["rollbackMsg"].(string))
+	s.logger.Debug(s.cache.GetString(keyRollbackMsg))
 	return s.RollbackPrev(ctx, prevFailure, report)
 }
 
 func (s *RestartContainers) Run(ctx context.Context, prevSuccess *automa.Success) (automa.Reports, error) {
 	report := automa.NewReport(s.ID)
-
-	s.logger.Debug(fmt.Sprintf("RUN - %q", s.ID))
-	s.cache["rollbackMsg"] = fmt.Sprintf("ROLLBACK - %q", s.ID)
+	s.cache.SetString(keyRollbackMsg, fmt.Sprintf("RUN - %q", s.ID))
 
 	// trigger rollback on error
 	err := errors.New("error running step 3")
@@ -96,7 +117,7 @@ func (s *RestartContainers) Run(ctx context.Context, prevSuccess *automa.Success
 
 func (s *RestartContainers) Rollback(ctx context.Context, prevFailure *automa.Failure) (automa.Reports, error) {
 	report := automa.NewReport(s.ID)
-	s.logger.Debug(s.cache["rollbackMsg"].(string))
+	s.logger.Debug(s.cache.GetString(keyRollbackMsg))
 	return s.RollbackPrev(ctx, prevFailure, report)
 }
 
